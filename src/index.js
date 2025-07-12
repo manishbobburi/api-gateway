bf57df34e38b83f1d39d1f99506da8a068704417
MANISH6148 <manishbobburi6148@gmail.com>
2025-07-12 21:43:35 +0530

    feat(index): add rate limiter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
 
 const app = express();
+
+const limiter = rateLimit({
+    window: 5 * 60 * 1000,
+    limit: 30,
+});
+
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
